c510507e8656ae206e56278ec339776b18a3f27a
student <nghuyduc2527@gmail.com>
2024-10-19 11:18:15 -0400

    404 page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -4,8 +4,10 @@ import "./NotFound.style.scss";
 function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
-      <h1>404</h1>
+      <h1>This page does not exist</h1>
+      <img src="/404.gif" alt="404 gif here" ></img>
     </Layout>
+    
   );
 }
 
