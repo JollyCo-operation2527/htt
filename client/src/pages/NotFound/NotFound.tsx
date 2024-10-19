30fce0ae332d341e09f0f7bb1f8a46d1d0153d2e
student <nghuyduc2527@gmail.com>
2024-10-19 11:22:39 -0400

    fix 404 page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -5,7 +5,7 @@ function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
       <h1>This page does not exist</h1>
-      <img src="/404.gif" alt="404 gif here" ></img>
+      <img src="https://cdn.dribbble.com/users/381530/screenshots/3949858/media/aff8c4541abddf91b8f69206b2175381.gif" alt="404 gif here" ></img>
     </Layout>
     
   );
